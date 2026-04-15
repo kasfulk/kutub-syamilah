e7d4dc53c426ec7e9493bcc8b1176a66b2a31470
Kasjful Kurniawan <kasful.bpp@gmail.com>
2026-04-15 23:59:09 +0800

    chore: remove obsolete database migration scripts and index initialization tools

--- ddl_tsvector.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE konten_kitab ADD COLUMN IF NOT EXISTS search_vector tsvector GENERATED ALWAYS AS (to_tsvector('arabic'::regconfig, isi_teks)) STORED;
-CREATE INDEX IF NOT EXISTS idx_konten_vector_search ON konten_kitab USING gin (search_vector);
